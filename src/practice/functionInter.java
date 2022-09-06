d5f14f25ea3826ae52d4d7a8b1055f2a506df074
aparajita <aparajitar202@gmail.com>
2022-09-06 07:45:16 +0530

    first commit to test

---
+++ src/practice/functionInter.java
@@ -0,0 +1,18 @@
+package practice;
+
+@FunctionalInterface
+public interface functionInter {
+
+    public abstract  void sayHello();
+
+  /*  public static  String sayHi(){
+        return "How are you STATIC method";
+    }
+
+    public default int letsPay()
+    {
+        System.out.println("retrun 10");
+        return 10;
+    }*/
+
+}
